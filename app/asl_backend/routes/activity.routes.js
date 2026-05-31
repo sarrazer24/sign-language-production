c15d76f2b5eb60ca3fb1439a8e1a4e94146999e9
nafissa <n.hallalbi@esi-sba.dz>
2026-05-31 15:42:12 -0600

    Remove asl_backend folder

--- app/asl_backend/routes/activity.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const ctrl   = require('../controllers/activity.controller');
-const auth   = require('../middleware/auth.middleware');
-
-router.get('/', auth, ctrl.getRecentActivity);
-
-module.exports = router;
--- app/asl_backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router  = require('express').Router();
-const ctrl    = require('../controllers/auth.controller');
-const auth    = require('../middleware/auth.middleware');
-
-router.post('/signup',  ctrl.signUp);
-router.post('/signin',  ctrl.signIn);
-router.get('/profile',  auth, ctrl.getProfile);
-
-module.exports = router;
